0984ffa1f13e6d760e110d79b6a73340d2e7a045
John Duddy <jduddy@illumina.com>
2012-04-02 17:56:07 -0700

    Got mostly working - last post is failing

--- ApiSampleApp/Controllers/HomeController.cs
+++ ApiSampleApp/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace ApiSampleApp.Controllers
 			return View();
 		}
 
-		public ActionResult Trigger(string actionuri, string returnuri,
+		public ActionResult Trigger(string action, string actionuri, string returnuri,
 			string error, string error_description, string state, string code)
 		{
 			if (null != actionuri)
@@ -39,14 +39,27 @@ namespace ApiSampleApp.Controllers
 			ViewBag.Message = string.Format("Auth failed: error: {0}, error_description: {1}", error, error_description);
 			return View("ShowRawText");
 		}
+		public void SetBasicAuthHeader(WebRequest request, String userName, String userPassword)
+		{
+			string authInfo = userName + ":" + userPassword;
+			authInfo = Convert.ToBase64String(System.Text.Encoding.Default.GetBytes(authInfo));
+			request.Headers["Authorization"] = "Basic " + authInfo;
+		}
 
 		private ActionResult HandleInitialTrigger(string actionuri, string returnuri)
 		{
 			try
 			{
 				// fetch the action info
-				var request = WebRequest.Create(actionuri);
+				string uri;
+				if (actionuri.ToLower().StartsWith("http"))
+					uri = actionuri;
+				else
+					uri = ConfigurationManager.AppSettings["BasespaceAppServerUri"] + actionuri;
+				var request = WebRequest.Create(uri);
 				request.ContentType = "application/json";
+				SetBasicAuthHeader(request, ConfigurationManager.AppSettings["MyClientId"],
+					ConfigurationManager.AppSettings["MyClientSecret"]);
 				var response = (HttpWebResponse)request.GetResponse();
 				string scope = null;
 					
@@ -59,8 +72,9 @@ namespace ApiSampleApp.Controllers
 				{
 					using (var stm = new StreamReader(response.GetResponseStream()))
 					{
+						var data = stm.ReadToEnd();
 						var json = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
-						var dict = (IDictionary<string, object>)json.DeserializeObject(stm.ReadToEnd());
+						var dict = (IDictionary<string, object>)json.DeserializeObject(data);
 
 						// store in local DB so that after the authentication, we can retrieve the request info
 						// for this demo, just put it in the in-memory cache
@@ -78,7 +92,7 @@ namespace ApiSampleApp.Controllers
 					var oauthUrl = string.Format("{0}?client_id={1}&redirect_uri={2}&response_type=code",
 						ConfigurationManager.AppSettings["OauthUri"],
 						ConfigurationManager.AppSettings["MyClientId"],
-						ConfigurationManager.AppSettings["MyWebRedirectUri"]
+						ConfigurationManager.AppSettings["MyRedirectUri"]
 						);
 					if (scope != null)
 					{
@@ -98,16 +112,26 @@ namespace ApiSampleApp.Controllers
 		public ActionResult HandleAuthApproved(string state, string code)
 		{
 			// we got the authorization code, now we need an access token
-			var oauthUri = string.Format("{0}?client_id={1}&redirect_uri={2}&grant_type=authorization_code&code={3}",
-						ConfigurationManager.AppSettings["OauthTokenUri"],
-						ConfigurationManager.AppSettings["MyClientId"],
-						ConfigurationManager.AppSettings["MyWebRedirectUri"],
-						code,
-						ConfigurationManager.AppSettings["MyClientSecret"]
-						);
+			var payload = new Dictionary<string, string>
+			{
+				{"client_id", ConfigurationManager.AppSettings["MyClientId"]}, 
+				{"redirect_uri", ConfigurationManager.AppSettings["MyRedirectUri"]},
+				{"grant_type", "authorization_code" },
+				{"code", code },
+				{"client_secret", ConfigurationManager.AppSettings["MyClientSecret"] }
+			};
+			var json = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
+			var data = json.Serialize(payload);
+			var oauthUri = ConfigurationManager.AppSettings["OauthTokenUri"];
 			var request = WebRequest.Create(oauthUri);
-			request.ContentType = "application/x-www-form-urlencoded";
+			request.ContentType = "application/json";
+			request.Accept = "application/json";
+			request.ContentLength = data.Length;
 			request.Method = "POST";
+			using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+			{
+				streamWriter.Write(data);
+			}
 			var response = (HttpWebResponse)request.GetResponse();
 
 			if (response.StatusCode != HttpStatusCode.OK)
@@ -118,8 +142,8 @@ namespace ApiSampleApp.Controllers
 			{
 				using (var stm = new StreamReader(response.GetResponseStream()))
 				{
-					var json = new JavaScriptSerializer() { MaxJsonLength = int.MaxValue };
-					var dict = (IDictionary<string, object>)json.DeserializeObject(stm.ReadToEnd());
+					data = stm.ReadToEnd();
+					var dict = (IDictionary<string, object>)json.DeserializeObject(data);
 
 					// store in local DB so that after the authentication, we can retrieve the request info
 					// for this demo, just put it in the in-memory cache
